40e9d9c869dea7d9e0bada8159cd9f41f47ec7f8
veronicanigro <veronica.nigro@yahoo.com>
2024-03-26 13:21:29 +0100

    version requirements and set up gpu in script

--- src/handler.py
+++ src/handler.py
@@ -19,13 +19,20 @@ from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
 
 
 def handler():
-    
-    sess = tf.Session()
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    for gpu in gpus: 
-        tf.config.experimental.set_memory_growth(gpu, True)
 
-    tf.config.list_physical_devices('GPU')
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+    # Create 2 virtual GPUs with 1GB memory each
+    try:
+        tf.config.set_logical_device_configuration(
+            gpus[0],
+            [tf.config.LogicalDeviceConfiguration(memory_limit=1024),
+            tf.config.LogicalDeviceConfiguration(memory_limit=1024)])
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPU,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        print(e)
     
     data = tf.keras.utils.image_dataset_from_directory('../data')
     data_iterator = data.as_numpy_iterator()
